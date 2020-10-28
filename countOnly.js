db25548a9577049b92b952fbf33b958e7b038be1
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-10-28 19:21:06 +0000

    Code for count only

---
+++ countOnly.js
@@ -0,0 +1,53 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !==${expected} `);
+  }
+
+};
+
+// TEST CODE
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+
+//function should take in a collection of items and return counts for a specific subset of those items
+//which items we care about and it will only count those, ignoring the others
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for(const item of allItems){
+    if(itemsToCount[item]){
+      if(results[item]){
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+//return an object containing counts of everything that the input object listed 
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
