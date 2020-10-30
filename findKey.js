012710fe4dedb190a5c93956acaf2805b0927748
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-10-30 02:35:49 +0000

    find key with calling function

---
+++ findKey.js
@@ -0,0 +1,46 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected} `);
+  }
+
+};
+
+// TEST CODE
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+
+
+const findKey = function(object1, value1){
+  
+  for(let key in object1){
+      if(value1(object1[key])){
+        return key;
+    }
+  } 
+  return undefined;
+};
+
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+const one1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+assertEqual('noma', one1)
+assertEqual(2, one1)
