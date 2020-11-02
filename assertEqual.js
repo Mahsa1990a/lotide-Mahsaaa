af726f0301621c8d8df11e38924ad2df297696db
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-11-02 19:20:47 +0000

    assert equal with Test

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,21 @@
 // FUNCTION IMPLEMENTATION
+
+//Concatenate(adds) :
+//const assertEqual = function(actual, expected) {
+ // if (actual === expected) {
+  //  console.log("✅✅✅Assertion Passed: " + actual + " === " + expected ); 
+ // } else {
+ //   console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected );
+ // }
+
+//};
+
+//Interpolated(injected) :
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected} `);
+    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected ); 
   } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !==${expected} `);
+    console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected );
   }
 
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
