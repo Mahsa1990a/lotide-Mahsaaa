a2789a765a1ca79fc0041e6c12201e971701b7a8
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-11-02 18:56:57 +0000

    head test with assertEqual

--- head.js
+++ head.js
@@ -1,25 +1,8 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  let head1 = '';
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !==${expected} `);
-  }
-};
-
+const assertEqual = require ('./assertEqual')
 const head = function(array1){
+  //return first index in array
   return array1[0];
 };
-//console.log([1, 2, 3, 4]); 
+head([1, 2, 3]);
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-
-
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
