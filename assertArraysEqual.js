819f068345b294563359707192d223f353c086e5
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-11-02 20:25:46 +0000

    testing code with module and export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,36 +1,5 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected} `);
-  }
-};
-// TEST CODE
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
-
-const eqArrays = function(array1, array2){
-  // if their length not same --> false
-  if(array1.length !== array2.length){
-    return false;
-  }
-  for(let i = 0 ; i < array1.length ; i++){
-    //if their element are not same return false
-    if(array1[i] !== array2[i]){
-      return false;
-    } //otherwise return true
-  } return true;
-};
-//Test code :
-//eqArrays([1, 2, 3], [1, 2, 3]) // => true
-//eqArrays([1, 2, 3], [3, 2, 1]) // => false
-//eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-//eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // => should not PASS
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true); 
-
+const assertEqual = require ('./assertEqual')
+const eqArrays = require ('./eqArrays')
 
 const assertArraysEqual = function(array1, array2){
   //using eqArrays function inside this function
@@ -42,5 +11,5 @@ const assertArraysEqual = function(array1, array2){
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3, 4]), true); // => should not PASS
+
+module.exports = assertArraysEqual;
